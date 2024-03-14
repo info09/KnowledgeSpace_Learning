1a838e877246a4c1e70dac7f18d663657c3bc910
VG-AMBER-HUYTQ\Tran Quang Huy <huytq@vega.com.vn>
2024-03-14 10:31:03 +0700

    API Role

---
+++ src/KnowledgeSpace.ViewModel/Pagination.cs
@@ -0,0 +1,8 @@
+﻿namespace KnowledgeSpace.ViewModel
+{
+    public class Pagination<T>
+    {
+        public List<T> Items { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
---
+++ src/KnowledgeSpace.ViewModel/Systems/Roles/RoleVm.cs
@@ -0,0 +1,9 @@
+﻿namespace KnowledgeSpace.ViewModel.Systems.Roles
+{
+    public class RoleVm
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
