e55a775354430c1a0ca1bbc11c13518aa7319487
VG-AMBER-HUYTQ\Tran Quang Huy <huytq@vega.com.vn>
2024-03-14 10:31:03 +0700

    API Role

---
+++ src/KnowledgeSpace.BackendServer/Controllers/RoleController.cs
@@ -0,0 +1,128 @@
+﻿using KnowledgeSpace.ViewModel;
+using KnowledgeSpace.ViewModel.Systems.Roles;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRoles()
+        {
+            var roles = await _roleManager.Roles.Select(i => new RoleVm()
+            {
+                Id = i.Id,
+                Name = i.Name
+            }).ToListAsync();
+            return Ok(roles);
+        }
+
+        [HttpGet("paging")]
+        public async Task<IActionResult> GetRoles(string? filter, int pageIndex, int pageSize)
+        {
+            var query = _roleManager.Roles;
+            if (!string.IsNullOrEmpty(filter))
+                query = query.Where(i => i.Id.Contains(filter) || i.Name.Contains(filter));
+
+            var totalRecord = await query.CountAsync();
+            var items = await query.Skip((pageIndex - 1) * pageSize).Take(pageSize).Select(i => new RoleVm() { Id = i.Id, Name = i.Name }).ToListAsync();
+
+            var pagination = new Pagination<RoleVm>
+            {
+                Items = items,
+                TotalRecords = totalRecord
+            };
+            return Ok(pagination);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+                return NotFound();
+
+            var roleVm = new RoleVm()
+            {
+                Id = role.Id,
+                Name = role.Name!
+            };
+            return Ok(roleVm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostRole(RoleVm roleVm)
+        {
+            var role = new IdentityRole()
+            {
+                Id = roleVm.Id,
+                Name = roleVm.Name,
+                NormalizedName = roleVm.Name.ToUpper(),
+            };
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return CreatedAtAction(nameof(GetById), new { id = role.Id }, roleVm);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRole(string id, [FromBody] RoleVm roleVm)
+        {
+            if (id != roleVm.Id)
+                return BadRequest();
+
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+                return NotFound();
+
+            role.Name = roleVm.Name;
+            role.NormalizedName = roleVm.Name.ToUpper();
+
+            var result = await _roleManager.UpdateAsync(role);
+
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+            return BadRequest(result.Errors);
+        }
+
+        //URL: DELETE: http://localhost:5001/api/roles/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+                return NotFound();
+
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                var rolevm = new RoleVm()
+                {
+                    Id = role.Id,
+                    Name = role.Name
+                };
+                return Ok(rolevm);
+            }
+            return BadRequest(result.Errors);
+        }
+    }
+}
