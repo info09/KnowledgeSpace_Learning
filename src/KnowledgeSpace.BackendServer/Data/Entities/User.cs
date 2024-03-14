4b26e4ffc40bc1c029b215a0c97688110f636155
VG-AMBER-HUYTQ\Tran Quang Huy <huytq@vega.com.vn>
2024-03-14 09:20:00 +0700

    add class

--- src/KnowledgeSpace.BackendServer/Data/Entities/User.cs
+++ src/KnowledgeSpace.BackendServer/Data/Entities/User.cs
@@ -1,6 +1,27 @@
-﻿namespace KnowledgeSpace.BackendServer.Data.Entities
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KnowledgeSpace.BackendServer.Data.Entities
 {
-    public class User
+    [Table("Users")]
+    public class User : IdentityUser
     {
+        [MaxLength(50)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime Dob { get; set; }
+
+        public int? NumberOfKnowledgeBases { get; set; }
+
+        public int? NumberOfVotes { get; set; }
+
+        public int? NumberOfReports { get; set; }
     }
 }
